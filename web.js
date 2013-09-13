03e2dca9454af526810b94261a4c99d0bfd3c07a
John Smith <example@gmail.com>
2013-09-13 04:47:05 +0000

    -

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(("hello again");
+  response.send("hello again");
 });
 
 var port = process.env.PORT || 5000;
